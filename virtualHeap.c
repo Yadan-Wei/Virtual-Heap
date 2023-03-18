8e359d57956d563c8b23197d4bb6fb6233786220
Yadan-Wei <yadanwei0712@gmail.com>
2023-03-17 22:53:22 -0700

    Add more test case

--- virtualHeap.c
+++ virtualHeap.c
@@ -6,12 +6,30 @@
 
 #include "virtualHeap.h"
 
-
 pthread_mutex_t vm_lock = PTHREAD_MUTEX_INITIALIZER;
 
-// init all physical blocks point to physical address
+/*
+Init physical blocks point to physical address
+init all virtual blocks
+Clean disk file.
+*/
 void pm_init()
 {
+
+    // open file in write mode
+    FILE *fp = fopen(FILEPATH, "w");
+    if (fp == NULL)
+    {
+        printf("Failed to open file.\n");
+        exit(1);
+    }
+
+    // write an empty string to the file
+    // so that the file is initialized
+    fwrite("", 1, 0, fp);
+
+    // close the file
+    fclose(fp);
 
     for (int i = 0; i < PHYSICAL_BLOCK_NUM; i++)
     {
@@ -167,11 +185,12 @@ Malloc funtcion return a pointer to virtual block
 */
 virtual_t *pm_malloc(size_t size)
 {
-    pthread_mutex_lock(&vm_lock);   // thread safety 
+    pthread_mutex_lock(&vm_lock); // thread safety
 
     if (is_virtual_full())
     {
-        pthread_mutex_unlock(&vm_lock);   // unlock the mutex, so other thread can access
+        printf("Virtual memory is full. Please check your memory usage.\n");
+        pthread_mutex_unlock(&vm_lock); // unlock the mutex, so other thread can access
         return NULL;
     }
     virtual_t *virtual_block = find_first_available_virtual_block();
@@ -246,13 +265,14 @@ void pm_free(virtual_t *virtual_block)
 /*
 Write with thread safety
 */
-void pm_write(virtual_t *virtual_block, char *string) {
+void pm_write(virtual_t *virtual_block, char *string)
+{
     pthread_mutex_lock(&vm_lock);
 
     // check if physical page present
     pm_check(virtual_block);
     // write to the physical address
-    char *paddr = virtual_block->physical->physical_addr;
+    char *paddr = (char *)(virtual_block->physical->physical_addr);
     strcpy(paddr, string);
 
     pthread_mutex_unlock(&vm_lock);
@@ -261,20 +281,21 @@ void pm_write(virtual_t *virtual_block, char *string) {
 /*
 Read with thread safety
 */
-char* pm_read(virtual_t *virtual_block) {
+char *pm_read(virtual_t *virtual_block)
+{
     pthread_mutex_lock(&vm_lock);
 
     // check if physical page present
     pm_check(virtual_block);
     // read starting at the physical address
-    char *paddr = virtual_block->physical->physical_addr;
-    
+    char *paddr = (char *)(virtual_block->physical->physical_addr);
+
     pthread_mutex_unlock(&vm_lock);
     return paddr;
 }
 
-
-int get_page_num(virtual_t *virtual_bock) {
+int get_page_num(virtual_t *virtual_bock)
+{
     // start physical address
     unsigned char *start = pm_heap;
     // current allocated physical address
--- vm_test.c
+++ vm_test.c
@@ -6,10 +6,14 @@
 
 #include "virtualHeap.h"
 
-void *thread_write_read_test(void *arg) {
-    int pid = *(int *)arg;  // thread id
+virtual_t *memories[VIRTUAL_BLOCK_NUM];
+
+void *thread_write_read_test(void *arg)
+{
+    int pid = *(int *)arg; // thread id
     virtual_t *vptr = pm_malloc(PAGE_SIZE);
-    if (!vptr) {
+    if (!vptr)
+    {
         printf("Virtual memory is full.\n");
         return NULL;
     }
@@ -18,24 +22,27 @@ void *thread_write_read_test(void *arg) {
     char *str = "Thread ";
     char cid = pid + '0';
     strcat(str, &cid);
-    
+
     pm_write(vptr, str);
 
     // Read from the physical memory
     char *paddr = pm_read(vptr);
     int str_size = strlen(str);
     printf("Read from ");
-    for (int i = 0; i < str_size; i++) {
+    for (int i = 0; i < str_size; i++)
+    {
         putchar(*paddr++);
     }
     printf("\n");
     return NULL;
 }
 
-void *thread_alloc_test(void *arg) {
+void *thread_alloc_test(void *arg)
+{
     int pid = *(int *)arg;
     virtual_t *vptr = pm_malloc(PAGE_SIZE);
-    if (!vptr) {
+    if (!vptr)
+    {
         printf("Virtual memory is full.\n");
         return NULL;
     }
@@ -46,97 +53,164 @@ void *thread_alloc_test(void *arg) {
     return NULL;
 }
 
+void test_single_thread_not_swap_malloc_free()
+{
+
+    for (int i = 0; i < PHYSICAL_BLOCK_NUM; i++)
+    {
+        memories[i] = pm_malloc(PAGE_SIZE);
+        if (!memories[i])
+        {
+            printf("Virtual memory is full.\n");
+            break;
+        }
+        printf("%d: Allocate to physical page #%d\n", i, get_page_num(memories[i]));
+        printf("%d Status: used %d, swapped: %d\n", i, memories[i]->used, memories[i]->swapped);
+    }
+
+    printf("\n");
+    printf("Free memory and check status.\n");
+
+    for (int i = 0; i < PHYSICAL_BLOCK_NUM; i++)
+    {
+        pm_free(memories[i]);
+        printf("%d Status: used %d, swapped: %d\n", i, memories[i]->used, memories[i]->swapped);
+    }
+    printf("\n");
+}
+
+void test_single_thread_swap_malloc_free()
+{
+    for (int i = 0; i < VIRTUAL_BLOCK_NUM; i++)
+    {
+        memories[i] = pm_malloc(PAGE_SIZE);
+        printf("%d: Allocate to physical page #%d\n", i, get_page_num(memories[i]));
+    }
+
+    printf("\n");
+    printf("Check status of all allocated memory.\n");
+
+    for (int i = 0; i < VIRTUAL_BLOCK_NUM; i++)
+    {
+        printf("%d Status: used %d, swapped: %d, offset: %ld\n", i, memories[i]->used, memories[i]->swapped, memories[i]->offset);
+    }
+
+    printf("\n");
+    printf("Free memory and check status.\n");
+
+    for (int i = 0; i < VIRTUAL_BLOCK_NUM; i++)
+    {
+        pm_free(memories[i]);
+        printf("%d Status: used %d, swapped: %d\n", i, memories[i]->used, memories[i]->swapped);
+    }
+}
+
+void test_single_thread_swap_write_read()
+{
+
+    for (int i = 0; i < VIRTUAL_BLOCK_NUM; i++)
+    {
+        memories[i] = pm_malloc(PAGE_SIZE);
+        if (!memories[i])
+        {
+            printf("Virtual memory is full.\n");
+            break;
+        }
+        printf("Write to number %d:\n", i);
+        char write_content[100] = "Hello CS5600 from number ";
+        char num_str[20];
+        sprintf(num_str, "%d", i);
+        strcat(write_content, num_str);
+        pm_write(memories[i], write_content);
+    }
+
+    printf("\n");
+    printf("Read from file or memory.\n");
+
+    for (int i = 0; i < VIRTUAL_BLOCK_NUM; i++)
+    {
+        if (!memories[i])
+        {
+            printf("Virtual memory is full.\n");
+            break;
+        }
+
+        printf("Before Read %d Status: used %d, swapped: %d\n", i, memories[i]->used, memories[i]->swapped);
+        printf("Read content is: %s\n", pm_read(memories[i]));
+        printf("After read %d Status: used %d, swapped: %d\n", i, memories[i]->used, memories[i]->swapped);
+        printf("\n");
+    }
+
+    printf("\n");
+    printf("Free memory and check status.\n");
+
+    for (int i = 0; i < VIRTUAL_BLOCK_NUM; i++)
+    {
+        pm_free(memories[i]);
+        printf("%d Status: used %d, swapped: %d\n", i, memories[i]->used, memories[i]->swapped);
+    }
+}
+
+void test_single_thread_memory_full()
+{
+    for (int i = 0; i < VIRTUAL_BLOCK_NUM; i++)
+    {
+        memories[i] = pm_malloc(PAGE_SIZE);
+        if (!memories[i])
+        {
+            printf("Virtual memory is full.\n");
+            break;
+        }
+        printf("%d: Allocate to physical page #%d\n", i, get_page_num(memories[i]));
+    }
+
+    virtual_t *v11 = pm_malloc(PAGE_SIZE);
+    if (v11)
+    {
+        printf("The virtual memory is not full.\n");
+    }
+    for (int i = 0; i < VIRTUAL_BLOCK_NUM; i++)
+    {
+        pm_free(memories[i]);
+    }
+}
+
 int main()
 {
     pm_init();
 
-    // Test for malloc and free in main thread
-    printf("******Test pm_malloc can assign more memory than actual physcial volume.*********\n");
-    virtual_t *v1 = pm_malloc(4 * 1024 * sizeof(char));
-    printf("v1 allocated physcial address is %p\n", v1->physical->physical_addr);
-    printf("v1 used: %d, v1 swapped: %d\n", v1->used, v1->swapped);
-    pm_check(v1);
-    char *a = (char *)(v1->physical->physical_addr);
-    strcpy(a, "Hello World!");
-    printf("the value assigned to v1 is %s\n", (char *)(v1->physical->physical_addr));
+    printf("\n\n");
+    printf("***********Test single thread not swap malloc and free.*********\n");
+    test_single_thread_not_swap_malloc_free();
 
-    virtual_t *v2 = pm_malloc(4 * 1024 * sizeof(char));
-    printf("v2 allocated physcial address is %p\n", v2->physical->physical_addr);
-    printf("v2 used: %d, v2 swapped: %d\n", v2->used, v2->swapped);
+    printf("\n\n");
+    printf("***********Test single thread allocate more than physcial, swap malloc and free.*******\n");
+    test_single_thread_swap_malloc_free();
 
-    pm_check(v2);
-    char *b = (char *)(v2->physical->physical_addr);
-    strcpy(b, "Hello CS5600!");
-    printf("the value assigned to v2 is %s\n", (char *)(v2->physical->physical_addr));
+    printf("\n\n");
+    printf("***********Test single thread read and write.***********\n");
+    test_single_thread_swap_write_read();
 
-    virtual_t *v3 = pm_malloc(4 * 1024 * sizeof(char));
-    printf("v3 allocated physcial address is %p\n", v3->physical->physical_addr);
-    printf("v3 used: %d, v3 swapped: %d\n", v3->used, v3->swapped);
-    virtual_t *v4 = pm_malloc(4 * 1024 * sizeof(char));
-    printf("v4 allocated physcial address is %p\n", v4->physical->physical_addr);
-    printf("v4 used: %d, v4 swapped: %d\n", v4->used, v4->swapped);
-    virtual_t *v5 = pm_malloc(4 * 1024 * sizeof(char));
-    printf("v5 allocated physcial address is %p\n", v5->physical->physical_addr);
-    printf("v5 used: %d, v5 swapped: %d\n", v5->used, v5->swapped);
-    virtual_t *v6 = pm_malloc(4 * 1024 * sizeof(char));
-    printf("v6 allocated physcial address is %p\n", v6->physical->physical_addr);
-    printf("v6 used: %d, v6 swapped: %d\n", v6->used, v6->swapped);
-    virtual_t *v7 = pm_malloc(4 * 1024 * sizeof(char));
-    printf("v7 allocated physcial address is %p\n", v7->physical->physical_addr);
-    printf("v7 used: %d, v7 swapped: %d\n", v7->used, v7->swapped);
+    printf("\n\n");
+    printf("************Test single thread memory is exhausted.***********\n");
+    test_single_thread_memory_full();
 
-    printf("*******After allocate v6, v7 check v1, v2 is swapped******\n");
-    printf("v1 used: %d, v1 swapped: %d, v1 offset: %ld\n", v1->used, v1->swapped, v1->offset);
-    printf("v2 used: %d, v2 swapped: %d, v2 offset: %ld\n", v2->used, v2->swapped, v2->offset);
-
-    printf("******Test reuse v1, v2 and swap in v1, v2 from file.*****\n");
-    pm_check(v1);
-    printf("v1 used: %d, v1 swapped: %d\n", v1->used, v1->swapped);
-    printf("the value assigned to v1 is %s\n", (char *)(v1->physical->physical_addr));
-
-    pm_check(v2);
-    printf("v2 used: %d, v2 swapped: %d\n", v2->used, v2->swapped);
-    printf("the value assigned to v2 is %s\n", (char *)(v2->physical->physical_addr));
-
-    printf("******Test pm_free() ******\n");
-
-    printf("Before free .....\n");
-    printf("v1 used: %d, v1 swapped: %d\n", v1->used, v1->swapped);
-    printf("v3 used: %d, v3 swapped: %d\n", v3->used, v3->swapped);
-    printf("After free .....\n");
-    pm_free(v1);
-    printf("v1 used: %d, v1 swapped: %d\n", v1->used, v1->swapped);
-    pm_free(v3);
-    printf("v3 used: %d, v3 swapped: %d\n", v3->used, v3->swapped);
-
-    virtual_t *v8 = pm_malloc(4 * 1024 * sizeof(char));
-    printf("v8 allocated physcial address is %p\n", v8->physical->physical_addr);
-    printf("v8 used: %d, v8 swapped: %d\n", v8->used, v8->swapped);
-    printf("\n");
-
-    // free allocated memory
-    pm_free(v2);
-    pm_free(v4);
-    pm_free(v5);
-    pm_free(v6);
-    pm_free(v7);
-    pm_free(v8);
-    
-    // Test for malloc and free with multi-threads 
+    // Test for malloc and free with multi-threads
     printf("Test for multi-threading malloc and free:\n");
     int threads_num = 12;
     pthread_t threads[threads_num];
 
-    for (int i = 0; i < threads_num; i++) {
+    for (int i = 0; i < threads_num; i++)
+    {
         int *index = (int *)malloc(sizeof(int));
         *index = i;
         pthread_create(&threads[i], NULL, &thread_alloc_test, index);
     }
 
-    for (int i = 0; i < threads_num; i++) {
+    for (int i = 0; i < threads_num; i++)
+    {
         pthread_join(threads[i], NULL);
     }
-
 
     // Test for read and write with multi-threads
     // printf("\nTest for multi-threading write and read:\n");
