d3bb43c81c421aec633959f7e9ff0dd49b39e993
qiaojunch <qiaojunch@gmail.com>
2023-03-18 00:20:32 -0700

    remove multi-threading test

--- vm_test.c
+++ vm_test.c
@@ -8,50 +8,7 @@
 
 virtual_t *memories[VIRTUAL_BLOCK_NUM];
 
-void *thread_write_read_test(void *arg)
-{
-    int pid = *(int *)arg; // thread id
-    virtual_t *vptr = pm_malloc(PAGE_SIZE);
-    if (!vptr)
-    {
-        printf("Virtual memory is full.\n");
-        return NULL;
-    }
 
-    // Write to the physical memory
-    char *str = "Thread ";
-    char cid = pid + '0';
-    strcat(str, &cid);
-
-    pm_write(vptr, str);
-
-    // Read from the physical memory
-    char *paddr = pm_read(vptr);
-    int str_size = strlen(str);
-    printf("Read from ");
-    for (int i = 0; i < str_size; i++)
-    {
-        putchar(*paddr++);
-    }
-    printf("\n");
-    return NULL;
-}
-
-void *thread_alloc_test(void *arg)
-{
-    int pid = *(int *)arg;
-    virtual_t *vptr = pm_malloc(PAGE_SIZE);
-    if (!vptr)
-    {
-        printf("Virtual memory is full.\n");
-        return NULL;
-    }
-    int page_num = get_page_num(vptr);
-    printf("Thread %d allocates page #%d\n", pid, page_num);
-
-    free(arg);
-    return NULL;
-}
 
 void test_single_thread_not_swap_malloc_free()
 {
@@ -196,33 +153,6 @@ int main()
     test_single_thread_memory_full();
 
     // Test for malloc and free with multi-threads
-    printf("Test for multi-threading malloc and free:\n");
-    int threads_num = 12;
-    pthread_t threads[threads_num];
-
-    for (int i = 0; i < threads_num; i++)
-    {
-        int *index = (int *)malloc(sizeof(int));
-        *index = i;
-        pthread_create(&threads[i], NULL, &thread_alloc_test, index);
-    }
-
-    for (int i = 0; i < threads_num; i++)
-    {
-        pthread_join(threads[i], NULL);
-    }
-
-    // Test for read and write with multi-threads
-    // printf("\nTest for multi-threading write and read:\n");
-    // for (int i = 0; i < threads_num; i++) {
-    //     int *index = (int *)malloc(sizeof(int));
-    //     *index = i;
-    //     pthread_create(&threads[i], NULL, &thread_write_read_test, index);
-    // }
-
-    // for (int i = 0; i < threads_num; i++) {
-    //     pthread_join(threads[i], NULL);
-    // }
 
     return 0;
 }
